6bf921170d7d96fba6018c3acf2a03ebcdbe5aba
bericontraster <bericontraster@gmail.com>
2026-09-22 16:50:14 -0400

    Sitemap Updates - SEO

--- _plugins/filter_sitemap.rb
+++ _plugins/filter_sitemap.rb
@@ -1,11 +1,29 @@
 Jekyll::Hooks.register :site, :post_write do |site|
   sitemap = File.join(site.dest, "sitemap.xml")
+
   if File.exist?(sitemap)
     content = File.read(sitemap)
+
     filtered = content.gsub(
-      %r{<url>\s*<loc>https?://[^<]*/(tags|categories|page\d+)/.*?</url>}m,
+      %r{
+        <url>\s*
+        <loc>https?://[^<]*/(?:tags|categories|page\d+|archives)(?:/[^<]*)?</loc>
+        .*?
+        </url>
+      }mx,
       ""
     )
+
+    filtered = filtered.gsub(
+      %r{
+        <url>\s*
+        <loc>https?://[^<]*/assets/muhammad-zubair-resume\.pdf</loc>
+        .*?
+        </url>
+      }mx,
+      ""
+    )
+
     File.write(sitemap, filtered)
   end
 end
